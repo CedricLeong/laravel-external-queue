79cee3b9ffc2769f5e2367188ffa1775bab995f1
cedric leong <cedricleong@gmail.com>
2018-10-16 14:03:19 -0400

    Update ExternalSqsJob.php

--- src/Jobs/ExternalSqsJob.php
+++ src/Jobs/ExternalSqsJob.php
@@ -18,7 +18,6 @@ class ExternalSqsJob extends SqsJob implements JobContract
 
         $handler = $this->resolveHandler();
         $data = $this->getJobData();
-        $this->delete();
         $handler->handle($this, $data);
     }
 
